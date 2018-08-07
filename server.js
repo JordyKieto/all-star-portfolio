2eb30dd7beb8d374ffd14362c588a35f165624cc
Jordy <kieto@ualberta.ca>
2018-08-07 15:19:30 -0400

    Added Resume

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require("express");
-const path = require('path')
 var app = express();
 
 app.use(express.static('static'))
 
-app.listen(process.env.PORT || 5000, ()=> console.log('Listening on...') )
+app.server = app.listen(process.env.PORT || 5000, ()=> console.log('Listening on...'+ app.server.address().port) )
 
