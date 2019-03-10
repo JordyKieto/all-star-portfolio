693336ff79d34949b2dd6ccffc974c6dc8896a79
unknown <kieto@ualbert.ca>
2019-03-10 11:25:28 -0400

    dir fix

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.use(express.static('static'))
-
-app.server = app.listen(process.env.PORT || 5000, ()=> console.log('Listening on...'+ app.server.address().port) )
-
